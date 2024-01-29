6af2d68ade9aee77bb1c0d193667601048a6aead
JBN303 <127194796+JBN303@users.noreply.github.com>
2024-01-29 08:27:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const db = require('./config/db');
+const doctorRoutes = require('./routes/doctorRoutes');
+const userRoutes = require('./routes/userRoutes');
+const patientRoutes = require('./routes/patientRoutes');
+const puserRoutes = require('./routes/puserRoutes')
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Use userRoutes for handling login
+app.use('/api', userRoutes);
+// Use doctorRoutes
+app.use('/api', doctorRoutes);
+// Use patientRoutes
+app.use('/api', patientRoutes);
+// Use puserRoutes...
+app.use('/api', puserRoutes);
+
+const PORT = process.env.PORT || 5007;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
